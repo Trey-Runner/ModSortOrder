b4b7db3e8877ce94b89923016d871007cfeaaf4d
Trey-Runner <trunner@students.kennesaw.edu>
2026-04-27 11:38:45 -0400

    Implement get_collection_mod_ids function
    Added a function to retrieve mod IDs from a Steam collection.

--- steam_api.py
+++ steam_api.py
@@ -1,10 +1,25 @@
-import os
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+def get_collection_mod_ids(collection_id):
+    url = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/"
 
-APP_ID = 294100
+    data = {
+        "collectioncount": 1,
+        "publishedfileids[0]": collection_id
+    }
+
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+
+    collection = response.json()["response"]["collectiondetails"][0]
+
+    mod_ids = []
+
+    for child in collection["children"]:
+        mod_ids.append(child["publishedfileid"])
+
+    return mod_ids
+
 
 def get_published_file_details(file_ids):
     url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/"
